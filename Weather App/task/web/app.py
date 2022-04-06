be0651f0ead491618871865ccb8ef69dca015436
parseltongist <>
2022-04-06 12:09:15 +0200

    Stage 3/5 stable version 'convert_to_celcius' function updated

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -13,8 +13,8 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-def convert_to_celcius(kelvin_value: float) -> float:
-    return kelvin_value - 273.15
+def convert_to_celcius(kelvin_value: float) -> int:
+    return int(round(kelvin_value - 273.15))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,6 +22,7 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == "POST":
+
         city_name = request.form.get('city_name')
         if not city_name:
             return render_template('index.html', weather=None)
@@ -31,11 +32,10 @@ def index():
         if r.status_code == 200:
             json_data = r.json()
             dict_with_weather_info = {
-                'temp': round(convert_to_celcius(float(json_data['main']['temp']))),
+                'temp': convert_to_celcius(float(json_data['main']['temp'])),
                 'state': json_data['weather'][0]['main'],
                 'name': json_data['name'],
             }
-            # TODO add save to file functionality?
             return render_template('index.html', weather=dict_with_weather_info)
         else:
             return render_template('index.html', weather=None)
