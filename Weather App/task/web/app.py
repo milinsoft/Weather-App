2adc161c71ca22bb04db1bebc6c76a11e055013c
parseltongist <>
2022-04-14 16:59:28 +0200

    Stage 5/5 working app

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -1,8 +1,9 @@
 import sys
 from datetime import datetime, timedelta
+from os import urandom
 
 import requests
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,9 +11,15 @@ app = Flask(__name__)
 API_KEY = 'a2eec5522c1c2c34cdf69f49e448083b'
 user_agent = {'User-agent': 'Mozilla/5.0'}
 DB_NAME = 'weather'
+SECRET_KEY = urandom(30)  # generating the secret key with length 30
 
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}.db'
+
+app.config.update(
+    {'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+     'SQLALCHEMY_DATABASE_URI': f'sqlite:///{DB_NAME}.db',
+     'SECRET_KEY': SECRET_KEY,
+     }
+)
 
 db = SQLAlchemy(app)
 
@@ -28,10 +35,6 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-def get_local_time(timezone: str) -> datetime:
-    return datetime.utcnow() + timedelta(seconds=int(timezone))
-
-
 def get_part_of_the_day(time_stamp: datetime) -> str:
     hour = time_stamp.hour
     parts_of_the_day = ((5, 12, 'morning'), (12, 17, 'afternoon'),
@@ -43,24 +46,40 @@ def get_part_of_the_day(time_stamp: datetime) -> str:
 
 
 def add_to_database(city_name, database=WeatherInCity):
+    def city_exists():
+        web_site = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}'
+        r = requests.get(web_site, headers=user_agent)
+        return r.status_code == 200
+
     if not city_name:
         pass
-    city = database(name=city_name)
-    already_in_database = database.query.filter(database.name == city_name).first()
-    if not already_in_database:
-        db.session.add(city)
-        db.session.commit()
+
+    # city validation:
+    if city_exists():
+        city = database(name=city_name)
+        already_in_database = database.query.filter(database.name == city_name).first()
+        if not already_in_database:
+            db.session.add(city)
+            db.session.commit()
+        else:
+            flash('The city has already been added to the list!')
+    else:
+        flash("The city doesn't exist!")
 
 
-def get_weather_from_api(city_name) -> dict:
+def get_weather_from_api(city_name, city_id) -> dict:
     def kelvin_to_celcius(kelvin_value: float) -> int:
         return int(round(kelvin_value - 273.15))
+
+    def get_local_time(timezone: str) -> datetime:
+        return datetime.utcnow() + timedelta(seconds=int(timezone))
 
     web_site = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}'
     r = requests.get(web_site, headers=user_agent)
     if r.status_code == 200:
         json_data = r.json()
         dict_with_weather_info = {
+            'id': city_id,
             'name': json_data['name'].upper(),
             'temp': kelvin_to_celcius(float(json_data['main']['temp'])),
             'state': json_data['weather'][0]['main'],
@@ -74,13 +93,13 @@ def get_forecast() -> list:
     cities_in_db = WeatherInCity.query.all()
     weather_data: list = []
     for city in cities_in_db:
-        data = get_weather_from_api(city.name)
+        data = get_weather_from_api(city.name, city.id)
         if data:
             weather_data.append(data)
     return weather_data
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         weather_data = get_forecast()
@@ -91,12 +110,20 @@ def index():
         return redirect(url_for('index'))  # name of the function
 
 
+@app.route('/delete/<int:city_id>', methods=['GET', 'POST'])
+def delete(city_id):
+    in_databse = WeatherInCity.query.filter_by(id=city_id).first()
+    db.session.delete(in_databse)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 # don't change the following way to run flask:
 if __name__ == '__main__':
     db.create_all()  # save the table in the database
     app.register_error_handler(404, page_not_found)
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
-        app.run(host=arg_host, port=arg_port, debug=True)
+        app.run(host=arg_host, port=arg_port, debug=False)
     else:
-        app.run(debug=True)
+        app.run(debug=False)
